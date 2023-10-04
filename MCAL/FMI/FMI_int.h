ba076e46db1660a7d8ba2ccba50682e478571e6b
jonathanmaged <105718364+jonathanmaged@users.noreply.github.com>
2023-10-04 22:01:55 +0200

    FLASH DRIVER

---
+++ MCAL/FMI/FMI_int.h
@@ -0,0 +1,17 @@
+#ifndef MCAL_FMI_FMI_INT_H_
+#define MCAL_FMI_FMI_INT_H_
+
+enum{
+	SECTOR0,
+	SECTOR1,
+	SECTOR2
+};
+
+void MFMI_vSectorErase(u8 A_u8SectorNumber);
+
+void MFMI_vFlashWrite( u32 A_u32Address,
+		              u16* A_pu16Data,
+					  u16 A_u16Length
+					  );
+
+#endif /* MCAL_FMI_FMI_INT_H_ */
